2d2f8e9a26c7c2f4ad91deac4667afc48b0f0cfd
Adam Egyed <adamegyed@gmail.com>
2022-02-10 20:59:10 -0800

    update default ratio change amount

--- src/data/constants/Values.ts
+++ src/data/constants/Values.ts
@@ -3,5 +3,5 @@ import Big from 'big.js';
 export const UINT256_MAX = '0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff';
 export const BLOCKS_TO_WAIT = 1;
 export const WETH_GAS_RESERVE = new Big('200000000000000000');
-export const DEFAULT_RATIO_CHANGE = '2.0';
+export const DEFAULT_RATIO_CHANGE = '5.0';
 export const RATIO_CHANGE_CUTOFF = 0;
