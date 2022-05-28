a243999c6998552a52417278bce592be95d5d9fd
Ian Woodard <ianwoodard2000@gmail.com>
2022-05-28 16:52:49 -0700

    Revamping the header and footer components

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import styled from 'styled-components';
 import tw from 'twin.macro';
-import AloeLogo from '../../assets/svg/aloe_capital_logo_no_border.svg';
+import AloeLogo from '../../assets/svg/aloe_capital_nav_logo.svg';
 import ConnectWalletButton from './ConnectWalletButton';
 
 type MenuItem = {
@@ -30,11 +30,18 @@ const MenuItems: MenuItem[] = [
 ];
 
 const StyledNavLink = styled(NavLink)`
+  ${tw`px-8 py-5`}
+  font-size: 16px;
+  font-weight: 400;
+  line-height: 24px;
+  color: rgba(75, 105, 128, 1);
   cursor: pointer;
-  font-weight: 300;
   user-select: none;
-  ${tw`text-grey-800`}
+
   &.active {
+    color: rgba(255, 255, 255, 1);
+  }
+  /* &.active {
     background: linear-gradient(
       180deg,
       rgba(15, 23, 42, 0.16) 0%,
@@ -44,7 +51,7 @@ const StyledNavLink = styled(NavLink)`
     border-bottom: 4px solid #59d67c;
     padding-top: 4px;
     ${tw`text-grey-1000`};
-  }
+  } */
 
   :hover:not(&.active) {
     ${tw`text-grey-900`};
@@ -52,31 +59,44 @@ const StyledNavLink = styled(NavLink)`
 `;
 
 const Nav = styled.nav`
-  box-shadow: 0px 1px 0px #223645;
+  ${tw`fixed top-0 left-0 right-0 z-10 flex items-center justify-between h-16`}
+  border-bottom: 1px solid rgba(26, 41, 52, 1);
+  background-color: rgba(6, 11, 15, 1);
+`;
+
+const VerticalDivider = styled.div`
+  width: 1px;
+  height: 64px;
+  background-color: rgba(26, 41, 52, 1);
 `;
 
 export default function Header() {
   return (
-    <Nav className='fixed top-0 left-0 right-0 z-10 flex items-center justify-between h-16 px-6 lg:px-20 bg-grey-25 border-b-2 border-b-grey-100'>
-      <div className='flex flex-row align-middle items-center'>
-        <a href='..' className='flex flex-row items-center justify-center'>
-          <img src={AloeLogo} className='h-9 w-auto p-2' alt='' />
-          {/* <span className='text-xl font-semibold'>Aloe</span> */}
-        </a>
+    <Nav>
+      <div className='flex'>
+        <div className='flex flex-row align-middle items-center px-8'>
+          <a href='..' className='flex flex-row items-center justify-center'>
+            <img src={AloeLogo} alt={'Aloe\'s Logo'} width={100} height={40} />
+            {/* <span className='text-xl font-semibold'>Aloe</span> */}
+          </a>
+        </div>
+        <VerticalDivider />
+        <div className='flex flex-row align-middle items-center h-full text-md'>
+          {MenuItems.map((menuitem) => (
+            <>
+              <StyledNavLink
+                id={`${menuitem.name}-nav-link`}
+                to={menuitem.url}
+                key={menuitem.name}
+              >
+                {menuitem.title}
+              </StyledNavLink>
+              <VerticalDivider />
+            </>
+          ))}
+        </div>
       </div>
-      <div className='flex flex-row align-middle items-center h-full text-md'>
-        {MenuItems.map((menuitem) => (
-          <StyledNavLink
-            id={`${menuitem.name}-nav-link`}
-            to={menuitem.url}
-            key={menuitem.name}
-            className='w-28 h-full flex flex-row items-center justify-center'
-          >
-            {menuitem.title}
-          </StyledNavLink>
-        ))}
-      </div>
-      <div className='p-2'>
+      <div className='mr-8'>
         <ConnectWalletButton />
       </div>
     </Nav>
