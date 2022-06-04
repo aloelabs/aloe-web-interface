ab16b0dbbd43337c3c7c9786afe060ead21caf45
Ian Woodard <17186604+IanWoodard@users.noreply.github.com>
2022-06-04 12:45:53 -0700

    Added graph to Blend Pool page (#4)
    * (WIP) Initial prototype of graphs
    
    * Refactored graph logic
    
    * Fixing dependencies
    
    * Updating component to better fit the spec
    
    * Fixing ordering
    
    * Fixing time selector position
    
    * Addressing comments
    
    * Final cleaning

--- src/data/constants/Breakpoints.ts
+++ src/data/constants/Breakpoints.ts
@@ -1,2 +1,3 @@
+export const RESPONSIVE_BREAKPOINT_LG = '1400px';
 export const RESPONSIVE_BREAKPOINT_MD = '1200px';
 export const RESPONSIVE_BREAKPOINT_SM = '768px';
--- src/pages/BlendPoolPage.tsx
+++ src/pages/BlendPoolPage.tsx
@@ -1,12 +1,13 @@
 import React, { useContext } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import PoolInteractionTabs from '../components/pool/PoolInteractionTabs';
-// import BlendAllocationGraph from '../components/allocationgraph/BlendAllocationGraph';
+import BlendAllocationGraph from '../components/allocationgraph/BlendAllocationGraph';
 import PoolStatsWidget from '../components/poolstats/PoolStatsWidget';
 import { BlendTableContext } from '../data/context/BlendTableContext';
 import { BlendPoolProvider } from '../data/context/BlendPoolContext';
 import PoolPieChartWidget from '../components/poolstats/PoolPieChartWidget';
 import styled from 'styled-components';
+import { RESPONSIVE_BREAKPOINT_LG, RESPONSIVE_BREAKPOINT_MD, RESPONSIVE_BREAKPOINT_SM } from '../data/constants/Breakpoints';
 import PoolPositionWidget from '../components/poolstats/PoolPositionWidget';
 import TokenPairHeader from '../components/pool/TokenPairHeader';
 import { GetTokenData } from '../data/TokenData';
@@ -24,9 +25,39 @@ type PoolParams = {
   pooladdress: string;
 };
 
+const PoolBodyWrapper = styled.div`
+  display: grid;
+  width: 100%;
+  grid-template-columns: calc(70% - 16px) calc(30% - 16px);
+  gap: 32px;
+  padding-left: 64px;
+  padding-right: 64px;
+
+  @media (max-width: ${RESPONSIVE_BREAKPOINT_LG}) {
+    grid-template-columns: calc(60% - 16px) calc(40% - 16px);
+  }
+
+  @media (max-width: ${RESPONSIVE_BREAKPOINT_MD}) {
+    grid-template-columns: 100%;
+    gap: 0;
+  }
+
+  @media (max-width: ${RESPONSIVE_BREAKPOINT_SM}) {
+    padding-left: 32px;
+    padding-right: 32px;
+  }
+`;
+
 const GridExpandingDiv = styled.div`
   grid-row: 1 / span 2;
   grid-column: 2 / span 1;
+
+  @media (max-width: ${RESPONSIVE_BREAKPOINT_MD}) {
+    grid-row: 2 / span 1;
+    grid-column: 1 / span 1;
+    margin-top: 96px;
+    margin-bottom: 32px;
+  }
 `;
 
 export default function BlendPoolPage() {
@@ -45,32 +76,27 @@ export default function BlendPoolPage() {
 
   return (
     <BlendPoolProvider poolData={poolData}>
-      <div className='flex flex-row justify-center'>
-        <div className='h-full max-w-[600px] md:max-w-[1260px] flex flex-row flex-nowrap gap-12 py-16 px-12 md:px-36'>
-          {/* max-w-screen-sm px-6 sm:px-10 md:px-20 */}
-          <div className='flex flex-col md:grid md:gap-x-10 md:grid-cols-[3fr_2fr]'>
-            <div className='flex items-center gap-8 relative'>
-              <PreviousPageButton onClick={() => navigate('../pools')} />
-              <TokenPairHeader
-                token0={GetTokenData(poolData.token0Address.toLowerCase())}
-                token1={GetTokenData(poolData.token1Address.toLowerCase())}
-                silo0={GetSiloData(poolData.silo0Address.toLowerCase())}
-                silo1={GetSiloData(poolData.silo1Address.toLowerCase())}
-              />
-              <AbsoluteFeeTierContainer feeTier={poolData.feeTier} />
-            </div>
-            <GridExpandingDiv className='w-full min-w-[340px] md:mt-24 sm:mt-24 md:grid-flow-row-dense'>
-              <PoolInteractionTabs poolData={poolData} />
-            </GridExpandingDiv>
-            <div className='w-full py-4 mt-16'>
-              {/*<BlendAllocationGraph />*/}
-              <PoolPositionWidget poolData={poolData} />
-              <PoolStatsWidget poolData={poolData} />
-              <PoolPieChartWidget poolData={poolData} />
-            </div>
-          </div>
+      <PoolBodyWrapper>
+        <div className='flex items-center gap-8 relative pt-16'>
+          <PreviousPageButton onClick={() => navigate('../pools')} />
+          <TokenPairHeader
+            token0={GetTokenData(poolData.token0Address.toLowerCase())}
+            token1={GetTokenData(poolData.token1Address.toLowerCase())}
+            silo0={GetSiloData(poolData.silo0Address.toLowerCase())}
+            silo1={GetSiloData(poolData.silo1Address.toLowerCase())}
+          />
+          <AbsoluteFeeTierContainer feeTier={poolData.feeTier} />
         </div>
-      </div>
+        <GridExpandingDiv className='w-full min-w-[340px] md:mt-24 md:grid-flow-row-dense'>
+          <PoolInteractionTabs poolData={poolData} />
+        </GridExpandingDiv>
+        <div className='w-full py-4'>
+          <BlendAllocationGraph />
+          <PoolPositionWidget poolData={poolData} />
+          <PoolStatsWidget poolData={poolData} />
+          <PoolPieChartWidget poolData={poolData} />
+        </div>
+      </PoolBodyWrapper>
     </BlendPoolProvider>
   );
 }
