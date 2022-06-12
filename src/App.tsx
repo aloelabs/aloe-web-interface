066623551f004fbb1f439ece59563682a212e3f6
Ian Woodard <ianwoodard2000@gmail.com>
2022-06-12 16:32:03 -0700

    Added temporary inputs example page

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ import GovernancePage from './pages/GovernancePage';
 import AppBody from './components/common/AppBody';
 import { RedirectPartialPath } from './util/RedirectPartialPath';
 import { BlendTableProvider } from './data/context/BlendTableContext';
+import InputExamplesPage from './pages/InputExamplesPage';
 
 function App() {
   return (
@@ -48,6 +49,7 @@ function App() {
                   </Route>
                   <Route path='/portfolio' element={<PortfolioPage />} />
                   <Route path='/governance' element={<GovernancePage />} />
+                  <Route path='/inputs' element={<InputExamplesPage />} />
                   <Route path='/' element={<Navigate replace to='/blend' />} />
                   <Route path='*' element={<Navigate to='/' />} />
                 </Routes>
