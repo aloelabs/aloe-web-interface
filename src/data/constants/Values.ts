0c9502046465494a47e5a0450f1930067ab42080
Adam Egyed <adamegyed@gmail.com>
2022-07-07 16:29:28 -0600

    Remove constant change

--- src/data/constants/Values.ts
+++ src/data/constants/Values.ts
@@ -5,6 +5,6 @@ export const UINT256_MAX =
 export const BLOCKS_TO_WAIT = 1;
 export const WETH_GAS_RESERVE = new Big('200000000000000000');
 export const DEFAULT_RATIO_CHANGE = '5.0';
-export const RATIO_CHANGE_CUTOFF = 1;
+export const RATIO_CHANGE_CUTOFF = 0;
 export const API_URL = 'http://34.94.221.78:3000';
 export const GAS_ESTIMATION_SCALING = 1.1;
