8579633ec41ab7e0354313238c5813430de66478
Adam Egyed <adamegyed@gmail.com>
2022-07-09 21:43:36 -0600

    Remove debug message

--- src/util/Env.ts
+++ src/util/Env.ts
@@ -2,8 +2,6 @@ const DEVMODE: boolean = (process.env.REACT_APP_ENV === "development");
 const IS_DEV = DEVMODE;
 const IS_PROD = !DEVMODE;
 
-console.log(`Devmode? ${IS_DEV}`);
-
 export {
   IS_DEV,
   IS_PROD,
