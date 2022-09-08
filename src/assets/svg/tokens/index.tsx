158cb48330a4f1e4a815f4e98a7c8c8ce6d6b074
Adam Egyed <adamegyed@gmail.com>
2022-09-08 16:32:50 -0400

    Add oSQTH logo and token data

--- src/assets/svg/tokens/index.tsx
+++ src/assets/svg/tokens/index.tsx
@@ -5,6 +5,7 @@ import WethLogo from './weth.svg';
 import TribeLogo from './tribe.svg';
 import RaiLogo from './rai.svg';
 import LooksLogo from './looks.svg';
+import OSqthLogo from './oSQTH.svg';
 import UnknownTokenLogo from './unknown_token.svg';
 
 export {
@@ -16,4 +17,5 @@ export {
   TribeLogo,
   RaiLogo,
   LooksLogo,
+  OSqthLogo,
 };
